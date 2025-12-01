635b4f908ce5a925c821584c1689b0bb6302e297
Kewang <cpckewang@gmail.com>
2025-12-01 19:38:02 +0800

    Remove default value for OUTAGE_KEYWORD

--- app_scripts/power_outage.js
+++ app_scripts/power_outage.js
@@ -7,7 +7,7 @@ const { sendToHA } = require("./mqtt_utils");
 const URL =
   "https://service.taipower.com.tw/branch/d101/xcnotice?xsmsid=0M242581316312033070";
 
-const OUTAGE_KEYWORD = process.env.OUTAGE_KEYWORD || "和豐街";
+const OUTAGE_KEYWORD = process.env.OUTAGE_KEYWORD;
 const TW_YEAR_OFFSET = 1911;
 
 const STATUS_CODE = {
